f5cb9fefaa6d3a04b2f6fccdcab636dc81de9c0a
Igor Hakcolt <hakretcode@gmail.com>
2022-10-10 17:44:45 -0300

    refactored design and implemented BEM architecture

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,34 @@
 import { CardFlipper, createCardsFromTechs } from "./game.js"
 
-const FRONT = "card_front"
-const BACK = "card_back"
+const INITIAL_BUTTON = "presentation__button"
+const GAME_BOARD = "game-board"
 const CARD = "card"
+const BACK = "card__back"
+const FRONT = "card__front"
+const IMG = "card__img"
 let cardFlipper = null
 let timer = null
 
 init()
 
-function init(){
-    const cards = createCardsFromTechs()
-    initializeBoard(cards)
+function init() {
+    const startButton = document.getElementById(INITIAL_BUTTON)
+    startButton.addEventListener("click", () => {
+        startButton.parentElement.style.display = "none"
+        document.getElementById("content").style.display = "block"
+        initializeBoard()
+    })
 }
 
-function initializeBoard(cards) {
-    
+function initializeBoard() {
+    const cards = createCardsFromTechs()
+
     updateMoveAndScore([0, 0])
-    
+
     cardFlipper = new CardFlipper()
     timer = setInterval(updateTimer, 1000)
 
-    const gameBoard = document.getElementById("gameBoard")
+    const gameBoard = document.getElementById(GAME_BOARD)
     gameBoard.innerHTML = ""
 
     cards.forEach(cardObject => {
@@ -51,8 +59,8 @@ function updateTimer() {
 function getTimeFormatted() {
     let seconds = cardFlipper.getSeconds()
 
-    const minutes = seconds < 60 ? 0: parseInt(seconds / 60)
-    const minutesFormatted = minutes < 10 ? `0${minutes}`: minutes
+    const minutes = seconds < 60 ? 0 : parseInt(seconds / 60)
+    const minutesFormatted = minutes < 10 ? `0${minutes}` : minutes
 
     seconds = seconds < 60 ? seconds : parseInt(seconds % 60)
     const secondsFormatted = seconds < 10 ? `0${seconds}` : seconds
@@ -70,13 +78,13 @@ function createCardContent(cardObject, cardElement) {
 function createCardFace(face, cardObject, cardElement) {
     const cardElementFace = document.createElement("div")
     cardElementFace.classList.add(face)
-    cardElementFace.innerHTML = (face == FRONT) ? "<img src='images/" + cardObject.icon + ".png' class='icon'>" : "&lt/&gt"
+    cardElementFace.innerHTML = (face == FRONT) ? "<img src='images/" + cardObject.icon + ".png' class='" + IMG + "'>" : "&lt/&gt"
     cardElement.appendChild(cardElementFace)
 }
 
 function flipCard() {
     if (!cardFlipper.isCardFlipped(this.id)) {
-        this.classList.add("flip")
+        this.classList.add("any_flip")
 
         if (cardFlipper.checkNull()) return
 
@@ -96,27 +104,28 @@ function flipCard() {
                 cardFlipper.clearCards()
                 const res = cardFlipper.increaseMovesOrScore(CardFlipper.DataType.MOVES)
                 updateMoveAndScore(res)
-                firstCardView.classList.remove("flip")
-                secondCardView.classList.remove("flip")
+                firstCardView.classList.remove("any_flip")
+                secondCardView.classList.remove("any_flip")
             }, 1000)
         }
     }
 }
 
 function gameFinish() {
-    const gameFinishView = document.getElementById("gameFinish")
+    clearInterval(timer)
+
+    const gameFinishView = document.getElementById("game-finish")
     gameFinishView.style.display = "flex"
 
     const data = document.getElementById("data").children
     const gameFinishData = gameFinishView.children[1].children
-    for (let index = 0; index < 3; index++) gameFinishData[index].innerHTML =  data[index].innerHTML
+    for (let index = 0; index < 3; index++) gameFinishData[index].innerHTML = data[index].innerHTML
 
     document.getElementById("restart").addEventListener("click", restart)
 }
 
-function restart() { 
-    const gameFinishView = document.getElementById("gameFinish")
+function restart() {
+    const gameFinishView = document.getElementById("game-finish")
     gameFinishView.style.display = "none"
-    clearInterval(timer)
-    init()
- }
+    initializeBoard()
+}
